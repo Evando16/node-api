1db8bd206901f692a7602bf9acf110980801732b
Evando Junior <evando@ciandt.com>
2017-11-22 00:54:26 -0200

    server on

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('Hi Evando');
+var server = app.listen(process.env.PORT || 8080, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
+
+app.get('/', function (req, res) {
+   res.send('Hello World - Node API');
+})
